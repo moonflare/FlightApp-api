3e055dbc2c20433b1a64597b64ef50324ab9370a
Diana Buras <burasdiana@hotmail.com>
2018-10-18 12:48:19 +0200

    [add] api routes for users and flights

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,15 @@
-// const flightsController = require('../controllers').flights;
+const flightsController = require('../controllers').flights;
+const usersController = require('../controllers').users;
 
 module.exports = (app) => {
   app.get('/api', (req, res) => res.status(200).send({
-    message: 'Welcome to the Todos API!',
+    message: 'Welcome to the Flights API!',
   }));
 
-  // app.post('/api/flights', flightsController.create);
-  // app.get('/api/flights', flightsController.list);
+   app.post('/api/flights', flightsController.create);
+   app.get('/api/flights', flightsController.list);
+   app.post('/api/users', usersController.create);
+   app.get('/api/users', usersController.list);
   // app.get('/api/flights/:todoId', flightsController.retrieve);
   // app.put('/api/flights/:todoId', flightsController.update);
   // app.delete('/api/flights/:todoId', flightsController.destroy);
